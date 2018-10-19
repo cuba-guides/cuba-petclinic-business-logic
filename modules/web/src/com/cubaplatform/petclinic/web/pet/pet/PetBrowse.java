2357c0a4c354535ab158def5e659c7a89222254d
Mario <mario@die-davids.de>
2018-10-19 09:13:05 +0200

    created business logic directly in controller: pet discount

--- modules/web/src/com/cubaplatform/petclinic/web/pet/pet/PetBrowse.java
+++ modules/web/src/com/cubaplatform/petclinic/web/pet/pet/PetBrowse.java
@@ -1,6 +1,31 @@
 package com.cubaplatform.petclinic.web.pet.pet;
 
+import com.cubaplatform.petclinic.entity.pet.Pet;
 import com.haulmont.cuba.gui.components.AbstractLookup;
+import com.haulmont.cuba.gui.components.GroupTable;
+
+import javax.inject.Inject;
 
 public class PetBrowse extends AbstractLookup {
+
+
+    @Inject
+    protected GroupTable<Pet> petsTable;
+
+    public void calculateDiscount() {
+
+        Pet pet = petsTable.getSingleSelected();
+
+        int discount = 0;
+
+        int visitAmount = pet.getVisits().size();
+        if (visitAmount > 10) {
+            discount = 10;
+        }
+        else if (visitAmount > 5) {
+            discount = 5;
+        }
+
+        showNotification("Discount for " + pet.getName() + ": " + discount + "%", NotificationType.TRAY);
+    }
 }
